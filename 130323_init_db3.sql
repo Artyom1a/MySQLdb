65557bbd6c1fdc469236b8242e9d314a7ec13f9d
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-13 18:00:40 +0300

    13.03

--- 130323_init_db3.sql
+++ 130323_init_db3.sql
@@ -10,3 +10,5 @@ author VARCHAR(50),
 price DECIMAL(8,2),
 amount INT
    );
+INSERT INTO book (title, author, price,amount) VALUES ('Мастер и Маргарита','Булгаков М.А.',670.99,3);
+SELECT * FROM book;
