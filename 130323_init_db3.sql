87758cccb2a3790644821b654499d0a674cef4bb
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-13 18:03:56 +0300

    13.03

--- 130323_init_db3.sql
+++ 130323_init_db3.sql
@@ -19,3 +19,16 @@ SELECT * FROM book;
 SELECT author,title,price FROM book;
 SELECT title as Название,author as Автор FROM book;
 SELECT title,amount,amount*1.65 as pack FROM book;
+
+SELECT title, price, 
+    (price*18/100)/(1+18/100) AS tax, 
+    price/(1+18/100) AS price_tax 
+FROM book;
+
+SELECT title, 
+    price, 
+    ROUND((price*18/100)/(1+18/100),2) AS tax, 
+    ROUND(price/(1+18/100),2) AS price_tax 
+FROM book;
+
+SELECT title,author,amount,ROUND(price*0.7,2) as new_price from book;
