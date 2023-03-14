ff4a9f5522ae181a4d1eb9e93f0f2c0c62e2d72d
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-14 12:06:58 +0300

    14.03

--- 130323_init_db3.sql
+++ 130323_init_db3.sql
@@ -55,3 +55,17 @@ SELECT title, author, price * amount AS total
 FROM book
 WHERE price * amount > 4000;
 SELECT author,title,price FROM book WHERE amount<10;
+
+SELECT title, author, price 
+FROM book
+WHERE price > 600 AND author = 'Булгаков М.А.';
+
+SELECT title, author, price 
+FROM book
+WHERE (author = 'Булгаков М.А.' OR author = 'Есенин С.А.') AND price > 600;
+
+SELECT title, author, price 
+FROM book
+WHERE author = 'Булгаков М.А.' OR author = 'Есенин С.А.' AND price > 600;
+
+SELECT title,author,price,amount from book where (price>500 or price>600) and price*amount>5000;
