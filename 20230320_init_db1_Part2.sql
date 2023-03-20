ef40d2f9a1214d3b03943c2f0129ed4a82b36b15
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-20 13:35:28 +0300

    feature2003

---
+++ 20230320_init_db1_Part2.sql
@@ -0,0 +1,84 @@
+-- Part2
+-- 1. Создайте таблицу "Users", которая будет содержать информацию о пользователях:
+-- идентификатор, имя пользователя, адрес электронной почты, хэш пароля, дата
+-- регистрации
+
+CREATE DATABASE IF NOT EXISTS part2;
+
+USE part2;
+
+CREATE TABLE IF NOT EXISTS users(
+ID INT PRIMARY KEY AUTO_INCREMENT,
+`NAME` VARCHAR(25),
+EMAIL VARCHAR(30) UNIQUE,
+PAROL  VARCHAR(30),
+DATAREGISTRATION DATE
+); 
+
+SELECT * FROM users;
+
+INSERT INTO users(`NAME`,EMAIL,PAROL,DATAREGISTRATION) VALUES("Artyom","Artyom@mail.ru","1234",now());
+INSERT INTO users(`NAME`,EMAIL,PAROL,DATAREGISTRATION) VALUES("Petr","Petr@mail.ru","1234",now());
+
+-- 2.
+-- Создайте таблицу "Orders", которая будет содержать информацию о заказах:
+-- идентификатор, идентификатор пользователя, дата заказа, статус заказа (новый,
+-- обработанный, отмененный
+
+
+CREATE TABLE IF NOT EXISTS Orders(
+ID INT PRIMARY KEY AUTO_INCREMENT,
+IDusers INT,
+DATAREGISTRATION DATE,
+STATUSPURCHASE VARCHAR(30),
+FOREIGN KEY (IDusers)  REFERENCES users (ID)
+); 
+
+SELECT * FROM Orders;
+
+INSERT INTO Orders(IDusers,DATAREGISTRATION,STATUSPURCHASE) VALUES(1,now(),"новый");
+
+
+-- 3.
+-- Напишите запрос для выбора всех пользователей, зарегистрированных в
+-- определенный день
+
+SELECT * FROM users
+WHERE DATAREGISTRATION = '2023-03-13';
+
+-- 4.
+-- Напишите запрос для выбора всех заказов, которые были сделаны
+-- определенным пользователем
+
+
+SELECT * FROM users
+INNER JOIN Orders
+ON users.id=Orders.IDusers
+WHERE users.email = 'Artyom@mail.ru';
+-- WHERE users.`NAME` = 'Petr'; Не работает?
+
+
+-- 5.
+-- Создайте таблицу "Products", которая будет содержать информацию о товарах:
+-- идентификатор, название, описание, цена
+
+CREATE TABLE IF NOT EXISTS Products(
+ID INT PRIMARY KEY AUTO_INCREMENT,
+IDOrders INT,
+NAMEPRODUCTS VARCHAR(30),
+DESCRIPTIONS VARCHAR(120),
+PRICE DECIMAL,
+FOREIGN KEY (IDORDERS) REFERENCES Orders (ID)
+); 
+
+SELECT * FROM PRODUCTS;
+-- 6.
+-- Напишите запрос для выбора всех заказов, которые содержат определенный
+-- товар
+
+INSERT INTO Products(IDOrders,NAMEPRODUCTS,DESCRIPTIONS,PRICE) VALUES(1,'LAPTOP','THIS IS THE BEST LAPTOP',100);
+
+SELECT * FROM Orders
+INNER JOIN Products
+ON Orders.ID=Products.IDOrders
+WHERE NAMEPRODUCTS = 'LAPTOP';
